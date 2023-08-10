3d1b5b5d564c75cfda708afa099d18e2e28c52fe
Lucas Cardoso <lucas.businesss@yahoo.com>
2023-08-10 11:04:43 -0300

    Apredizado RBR

---
+++ 00_curso_4hrs.rb
@@ -0,0 +1,4 @@
+#usando o git
+#lembrar de deixar o terminal ja a pasta que vc esta fazendo os trem "cd/..."
+#entrar no git, criar um repositorio, ver o passo a passo. lembrar do 'git add .'
+#atualizando o q vc fez basta o commit -m "oq vc digitou", "git push -u original main"
