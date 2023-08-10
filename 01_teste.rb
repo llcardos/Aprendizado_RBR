60e7773d2ba389a1991b6d847e555b527d3f1037
Lucas Cardoso <lucas.businesss@yahoo.com>
2023-08-10 10:10:05 -0300

    Apredizado RBR

---
+++ 01_teste.rb
@@ -0,0 +1,13 @@
+puts "Digite seu nome"
+nome = gets.chomp
+puts "O seu nome é: " + nome
+
+puts "========================"
+
+puts nome.inspect
+
+puts "========================"
+
+puts " Digite seu salário"
+sal = gets.chomp.to_f
+puts " O seu salário atualizado é: " + (sal * 1.135).to_s
---
+++ 03_repetição.rb
@@ -0,0 +1,15 @@
+#each
+
+['A','b',3].each do |i|
+    puts " o valor lido foi: " + i.to_s
+end
+
+#while (lembra muito uma Progressao Aritimética)
+
+i = 0
+num = 5
+
+while i <= num do
+    puts "Contando... " + i.to_s
+    i += 1
+end
