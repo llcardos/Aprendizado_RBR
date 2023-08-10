cf85aa91fd5a6fa9c9fbf259e66c56631a7f983f
Lucas Cardoso <lucas.businesss@yahoo.com>
2023-08-10 11:47:32 -0300

    Apredizado RBR

---
+++ 05_hashes.rb
@@ -0,0 +1,6 @@
+#determina qual chave vai ser cada ponto do vetor, corresponde ao indice... v[indice1,indice2], com o rashes eu denomino o nome de cada indice.
+# "=>" foi subtituído por ":"
+
+h = {"x": 15, "cursos": "rails"}
+
+puts h["cursos"]
