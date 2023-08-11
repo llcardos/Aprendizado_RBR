a2ff0585bdfbd8de22711fa48f8c0a6ce11a1ada
Lucas Cardoso <lucas.businesss@yahoo.com>
2023-08-10 21:53:20 -0300

    Aprendizado-RBR

--- 05_hashes.rb
+++ 05_hashes.rb
@@ -1,6 +1,7 @@
 #determina qual chave vai ser cada ponto do vetor, corresponde ao indice... v[indice1,indice2], com o rashes eu denomino o nome de cada indice.
 # "=>" foi subtituído por ":"
 
-h = {"x": 15, "cursos": "rails"}
+h = {x: 15, cursos: "rails"}
 
-puts h["cursos"]
+puts h[:x]
+
