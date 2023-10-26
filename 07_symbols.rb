5b9e17ff1ecc9bbab8cbe0755d3316fc6a580bd9
Lucas Cardoso <lucas.businesss@yahoo.com>
2023-10-26 20:53:43 -0300

    Aprendizado RBR

---
+++ 07_symbols.rb
@@ -0,0 +1,17 @@
+puts "Lucas".object_id
+puts "Lucas".object_id
+puts "Lucas".object_id
+puts "Lucas".object_id
+
+puts :Lucas.object_id
+puts :Lucas.object_id
+puts :Lucas.object_id
+puts :Lucas.object_id
+
+puts "-----------------------"
+
+#hashes
+
+h = { :curso => "rails" } #antigo
+
+h = {curso: "rails"} #novo
