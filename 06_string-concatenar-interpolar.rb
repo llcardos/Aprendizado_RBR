39064dba137655e0fb3a3def929e44c796bf9d02
Lucas Cardoso <lucas.businesss@yahoo.com>
2023-10-26 20:36:57 -0300

    Aprendizado RBR

---
+++ 06_string-concatenar-interpolar.rb
@@ -0,0 +1,34 @@
+x = "Lucas"
+y = "Rails"
+
+puts x
+puts x.class
+puts y
+puts y.class
+
+a = "Curso"
+b = "Ruby"
+
+puts a << b
+puts a + b
+
+puts "-----------------------"
+
+x = "curso"
+puts x.object_id
+x = x + "rails"
+puts x
+puts x.object_id
+
+puts "-----------------------"
+
+q = "curso de "
+puts q.object_id
+q << "rails"
+puts q
+puts q.object_id
+
+puts "-----------------------"
+
+h = "Lucas #{q} Cardoso #{y}"
+puts h
